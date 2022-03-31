1346df938663e0a0dd456e1ccfd8ae72c5b62d51
R.Utochkin <utochkin.re@gmail.com>
2022-03-31 21:07:02 +0300

    Example D3.3_2

--- NewsPaper/news/templatetags/_filters.py
+++ NewsPaper/news/templatetags/_filters.py
@@ -4,9 +4,11 @@ register = template.Library()
 
 @register.filter()
 def censor(value):
+   if not isinstance(value, str):
+      raise ValueError('Нельзя цензурировать не строку')
    value = value.lower()
    exceptional_words = ['sometext', 'sometitle']
    for i in exceptional_words:
-      value = value.replace(i, '*'*len(i))
+      value = value.replace(i, '*'*(len(i)-1))
       value = value.replace(i.capitalize(), '*'*len(i))
    return f'{value}'
